e241202e8a8a14b28b5ca1a736bf5797a168dbd6
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2019-11-23 14:50:56 -0600

    [add] minus function

--- sesion1/js/index.js
+++ sesion1/js/index.js
@@ -2,6 +2,7 @@ let titulo = document.querySelector('h1')
 let primerNumero = document.querySelector('#primer-numero')
 let segundoNumero = document.querySelector('#segundo-numero')
 let sumButton = document.querySelector('#sum-button')
+let minusButton = document.querySelector('#minus-button')
 let resultNode = document.querySelector('#result')
 
 sumButton.addEventListener('click', function () {
@@ -10,6 +11,25 @@ sumButton.addEventListener('click', function () {
     let parsedNum2 = parseInt(segundoNumero.value)
     // suma de enteros
     let result = parsedNum1 + parsedNum2
+    // creación de nodo <span>{resultado}</span>
+    let textConten = document.createTextNode(result)
+    let spanElement = document.createElement('span')
+    spanElement.appendChild(textConten)
+    // eliminar cuando ya haya resultado
+    if (resultNode.childNodes.length > 3) {
+        resultNode.removeChild(resultNode.childNodes[3])
+    }
+    // agregar resultado a pantalla
+    resultNode.appendChild(spanElement)
+    return result
+})
+
+minusButton.addEventListener('click', function () {
+    // parseo a enteros (por defecto son texto)
+    let parsedNum1 = parseInt(primerNumero.value)
+    let parsedNum2 = parseInt(segundoNumero.value)
+    // resta de enteros
+    let result = parsedNum1 - parsedNum2
     // creación de nodo <span>{resultado}</span>
     let textConten = document.createTextNode(result)
     let spanElement = document.createElement('span')
