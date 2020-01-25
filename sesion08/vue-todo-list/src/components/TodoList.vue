7a799187af2991c153b1533c115751c6608e9b6e
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-01-25 10:09:10 -0600

    [add] todo list skeleton

---
+++ sesion08/vue-todo-list/src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h1>Todo list</h1>
+      <TodoItem/>
+  </div>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem'
+export default {
+    name: 'TodoList',
+    components: {
+        TodoItem
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ sesion08/vue-todo-list/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+      <h1>Home view</h1>
+      <TodoList/>
+  </section>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+
+export default {
+    name: 'Home',
+    components:{
+        TodoList
+    }
+}
+</script>
+
+<style>
+
+</style>
