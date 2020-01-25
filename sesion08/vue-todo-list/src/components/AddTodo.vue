5d38153dc00487582085f954e9873c07f2c6555e
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-01-25 15:28:36 -0600

    [add] Add Todo form component

---
+++ sesion08/vue-todo-list/src/components/AddTodo.vue
@@ -0,0 +1,37 @@
+<template>
+  <form v-on:submit="addTodo" >
+      <div>
+          <input
+            type="text"
+            name="addTodo"
+            placeholder="Add ToDo task..."
+            v-model="title">
+      </div>
+  </form>
+</template>
+
+<script>
+export default {
+    name: 'AddTodo',
+    data(){
+        return {
+            title: ''
+        }
+    },
+    methods: {
+      addTodo(e){
+        e.preventDefault();
+        const newTodoTask = {
+          title: this.title,
+          completed: false
+        }
+        this.$emit('add-todo', newTodoTask)
+        this.title = ''
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
