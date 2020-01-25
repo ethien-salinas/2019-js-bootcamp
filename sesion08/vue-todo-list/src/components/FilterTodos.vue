8eb8a28f4e5e0923f76644017a61633910e31611
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-01-25 15:29:00 -0600

    [add] Filter Todos buttons group component

---
+++ sesion08/vue-todo-list/src/components/FilterTodos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <button v-on:click="$emit('all-todo')">All</button>
+      <button v-on:click="$emit('active-todo')">Active</button>
+      <button v-on:click="$emit('completed-todo')">Completed</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
