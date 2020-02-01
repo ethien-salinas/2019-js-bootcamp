abc82d1fe787007e8ff7004580ec4eec1a253587
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-02-01 13:10:32 -0600

    build body data by events

--- sesion09/server/src/server.js
+++ sesion09/server/src/server.js
@@ -11,8 +11,20 @@ const headers = {
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.writeHead(200, headers);
-  console.log(req)
-  // hacer que solo responda /login cuando sea una llamada POST
+
+  // build body data by events
+  let body = [];
+  req.on('error', (err) => {
+    console.error(err);
+  })
+  .on('data', (chunk) => {
+    body.push(chunk);
+  })
+  .on('end', () => {
+    body = Buffer.concat(body).toString();
+    console.log(body)
+  })
+
   if(req.method === 'POST' && req.url === '/login'){
     res.end('[POST] /login path');
   }
@@ -22,3 +34,13 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+/**
+ * let body = [];
+  request.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+ */
