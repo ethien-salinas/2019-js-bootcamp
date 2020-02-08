966c6a815c34887719bd424fae85c567df6c7afa
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-02-08 10:20:20 -0600

    [add] router guards

--- sesion09/vue-router-example/src/router/index.js
+++ sesion09/vue-router-example/src/router/index.js
@@ -18,19 +18,69 @@ let router = new Router({
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      meta: {
+        guest: true
+      }
     },
     {
       path: '/admin',
       name: 'admin',
-      component: Admin
+      component: Admin,
+      meta: {
+        requiresAuth: true,
+        is_admin: true
+      }
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: Dashboard
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // ruta requiere authentication
+  if(to.matched.some(record => record.meta.requiresAuth)){
+    // aún no hay jwt, lo enviamos a /login
+    if(localStorage.getItem('jwt') === null){
+      next({
+        path: '/login',
+        params: { nextUrl: to.fullPath}
+      })
+    } else {
+      // recuperamos user de localStorage
+      let user = JSON.parse(localStorage.getItem('user'))
+      // ¿es vista excluisiva de administradores?
+      if(to.matched.some(record => record.meta.is_admin)){
+        if(user.is_admin === true){
+          next()
+        } else {
+          next({ name: 'dashboard'})
+        }
+      } else {
+        next()
+      }
+    }
+  }
+  // ruta visible para invitados
+  else if(to.matched.some(record => record.meta.guest)){
+    // invitado que no tiene jwt almacenado
+    if(localStorage.getItem('jwt') === null){
+      next()
+    } else {
+      next({ name: 'dashboard'})
+    }
+  }
+  // en general
+  else {
+    next()
+  }
+})
+
 
 export default router
