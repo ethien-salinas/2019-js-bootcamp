d23941c9b541d60232bb1ba3560a2891813d62ba
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-02-22 09:17:18 -0600

    [add] remote call

--- sesion11/remote-call-react/src/App.js
+++ sesion11/remote-call-react/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todo from './components/Todo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    remoteItem: null
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+      .then(response => response.json())
+      .then(json => this.setState({
+        remoteItem: json
+      }))
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.remoteItem &&
+          this.state.remoteItem.map(
+            item => <Todo key={item.id} title={item.title} />
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
