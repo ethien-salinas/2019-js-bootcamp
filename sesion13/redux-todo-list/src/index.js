b976d38e3c031a5282caa7f8edae0a2cc994520f
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-02-29 13:34:55 -0600

    [add] redux devtool support

--- sesion13/redux-todo-list/src/index.js
+++ sesion13/redux-todo-list/src/index.js
@@ -7,7 +7,10 @@ import './css/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer)
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
     <Provider store={store}>
