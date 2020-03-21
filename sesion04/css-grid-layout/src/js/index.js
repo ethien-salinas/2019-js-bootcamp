254b9f974e5d23f32d60ffb465f1c78736d03fb4
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-03-20 23:31:50 -0600

    [fix] typo error

--- sesion04/css-grid-layout/src/js/index.js
+++ sesion04/css-grid-layout/src/js/index.js
@@ -1,5 +1,5 @@
 import '../css/styles.css'
 
-  (process.env.NODE_ENV !== 'prodiction')
+  (process.env.NODE_ENV !== 'production')
   ? () => console.log('Development mode 💻👊🏻')
   : () => console.log('Production mode 🚀')
