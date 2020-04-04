561be15388cc398c034aa3a4a905f1277182e4eb
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-04-04 12:06:17 -0600

    [add] logger configuration

---
+++ sesion16/auth-graphql-api/src/index.js
@@ -0,0 +1,6 @@
+const logger = require('./logger')
+
+logger.info('This is a "info" line')
+logger.error('This is a "error" line')
+logger.debug('This is a "debug" line')
+
---
+++ sesion16/auth-graphql-api/src/logger/index.js
@@ -0,0 +1,46 @@
+const { createLogger, format, transports } = require('winston')
+const DailyRotateFile = require('winston-daily-rotate-file')
+const { timestamp, errors, printf } = format
+
+const logger = createLogger({
+  level: 'debug',
+  format: format.combine(
+    timestamp({
+      format: 'YYYY-MM-DDTHH:mm:ss'
+    }),
+    errors({ stack: true }),
+    printf(({ timestamp, level, message }) => {
+      return `${timestamp} | ${level} | ${message}`
+    })
+  ),
+  transports:[
+    // new transports.File({ filename: 'error.log', level: 'error' }),
+    // new transports.File({ filename: 'combined.log' })
+    new (DailyRotateFile)({
+      filename: './logs/application-error-%DATE%.log',
+      level: 'error',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d'
+    }),
+    new (DailyRotateFile)({
+      filename: './logs/application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d'
+    })
+  ]
+})
+
+if(process.env.NODE_ENV !== 'production'){
+  logger.add(new transports.Console({
+    format: format.combine(
+      format.colorize(),
+      format.simple()
+    )
+  }))
+}
+
+module.exports = logger
