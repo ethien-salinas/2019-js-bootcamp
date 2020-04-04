754c7ced6e56f5aed9bcd6a8462c0a57198dbb1e
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-04-04 13:20:27 -0600

    [add] bcrypt

--- sesion16/auth-graphql-api/src/index.js
+++ sesion16/auth-graphql-api/src/index.js
@@ -1,6 +1,16 @@
-const logger = require('./logger')
+import bcrypt from 'bcrypt'
+import logger from './logger'
 
-logger.info('This is a "info" line')
-logger.error('This is a "error" line')
-logger.debug('This is a "debug" line')
+const saltRounds = 9
+const myPlaintextPassword = 's0/\/\P4$$w0rD'
+const hash = '$2b$09$sAYBi0zu3E1f9xcKc1XT6uDIUmO.W.UL8nFzlHldIvK7cmuFyAC2q'
 
+bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+    if(err) logger.error(err)
+    logger.debug(`hash: ${hash}`)
+});
+
+bcrypt.compare(myPlaintextPassword, hash, function (err, result) {
+    if (err) logger.error(err)
+    logger.debug(`result: ${result}`)
+})
