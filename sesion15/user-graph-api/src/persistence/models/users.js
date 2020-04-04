8caba3d19df70d1dd5a3d6d44298765fb021ca60
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-04-03 23:48:59 -0600

    [update] project renamed to users api

--- sesion15/user-graph-api/src/persistence/models/users.js
+++ sesion15/user-graph-api/src/persistence/models/users.js
@@ -2,11 +2,11 @@ module.exports = (sequelize, DataTypes) => sequelize.define('users', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
-    autoIncrement: true
+    autoIncrement: true,
   },
   name: DataTypes.STRING,
   email: DataTypes.STRING,
   password: DataTypes.STRING,
   createdAt: DataTypes.DATE,
-  updatedAt: DataTypes.DATE,
-});
+  updatedAt: DataTypes.DATE
+})
