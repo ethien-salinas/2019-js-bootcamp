46d369e34ac4a3acb9a096cdcb236f9889ffaa65
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-04-18 13:02:01 -0500

    [add] isValidToken query

--- sesion16/auth-graphql-api/src/resolvers.js
+++ sesion16/auth-graphql-api/src/resolvers.js
@@ -1,11 +1,13 @@
 export default {
   Query: {
     login: async (_, { email, password }, { dataSources }) =>
-      await dataSources.authAPI.getToken({ email, password })
+      await dataSources.authAPI.getToken({ email, password }),
+    isValidToken: async (_, { token }, { dataSources }) =>
+      await dataSources.authAPI.verifyToken(token)
   },
 
   Mutation: {
-    signup: async(_, { name, email, password }, { dataSources }) => {
+    signup: async (_, { name, email, password }, { dataSources }) => {
       return dataSources.userAPI.createUser({ name, email, password })
     }
   }
--- sesion16/auth-graphql-api/src/schema.js
+++ sesion16/auth-graphql-api/src/schema.js
@@ -3,6 +3,7 @@ import { gql } from 'apollo-server'
 export default gql`
     type Query{
       login(email: String, password: String): String
+      isValidToken(token:String):Boolean
     }
     type Mutation {
       signup(name: String, email: String, password:String): User
