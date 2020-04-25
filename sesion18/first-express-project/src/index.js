5e95ed2a5d939c248525ded589f29cb5f3bf9d69
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-04-25 10:37:24 -0500

    [add] nodemon

--- sesion18/first-express-project/src/index.js
+++ sesion18/first-express-project/src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.end('*** Hello world with express ***')
+  res.end('*** Hello world ***')
 })
 
 app.listen(port, () => {
