b16c28a1e73b65121ccb23352253aa18e5fd9c71
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-05-23 10:15:28 -0500

    [fix] classNameusage

--- sesion20-22/react-dashboard/src/components/KpiItem.js
+++ sesion20-22/react-dashboard/src/components/KpiItem.js
@@ -3,10 +3,10 @@ import React from 'react'
 const KpiItem = (props) => {
   const { id, amount, title, class:className } = props.kpi
   return (
-    <div class="column is-one-quarter">
+    <div className="column is-one-quarter">
       <div className={`${className} notification has-text`}>
-        <p class="title is-1">{amount}</p>
-        <p class="subtitle is-4">{title}</p>
+        <p className="title is-1">{amount}</p>
+        <p className="subtitle is-4">{title}</p>
       </div>
     </div>
   )
