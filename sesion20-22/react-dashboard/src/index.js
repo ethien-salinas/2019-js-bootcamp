5a4aa6253df17daffbabaf303b9def3e24e3f325
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-05-23 11:36:41 -0500

    [add] use of env variable

--- sesion20-22/react-dashboard/src/index.js
+++ sesion20-22/react-dashboard/src/index.js
@@ -8,7 +8,7 @@ import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000'
+  uri: process.env.REACT_APP_GRAPHQL_SERVER
 })
 
 ReactDOM.render(
