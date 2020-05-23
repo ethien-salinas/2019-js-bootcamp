2ccce9267d973cbfd62164f2af725a5bcfb763ef
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-05-23 10:16:57 -0500

    [add] apollo client

--- sesion20-22/react-dashboard/src/index.js
+++ sesion20-22/react-dashboard/src/index.js
@@ -4,15 +4,20 @@ import './css/index.css';
 import './sass/styles.scss'
 import App from './view/App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000'
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
